636196db8fd61d5743267f7bcaa38ffcb0a8eb7a
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-01-24 10:27:49 -0300

    Feat: signOut, Style: navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { auth, signIn } from 'auth'
+import { auth, signIn, signOut } from 'auth'
 import Link from 'next/link'
 import React from 'react'
 
@@ -6,22 +6,30 @@ export default async function Navbar() {
 
     const session = await auth()
   return (
-    <div>
-        <Link href={"/"}>
+    <div className='bg-gray-800 text-white p-4 flex justify-between items-center'>
+        <Link href={"/"} className='text-white text-lg font-bold'>
             Home
         </Link>
 
-        <div>
+        <div className='flex gap-4 items-center'>
             {session && session.user ? (
                 <div>
                     <p>{session.user.name}</p>
+                    <form action={async () => {
+                    "use server"
+                    await signOut()
+                }}>
+                    <button className='bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded'>
+                        Sair
+                    </button>
+                </form>
                 </div>
             ) : (
                 <form action={async () => {
                     "use server"
                     await signIn()
                 }}>
-                    <button>
+                    <button className='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded'>
                         Entrar
                     </button>
                 </form>
