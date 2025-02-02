c7c9a9f24a06f2efd970708efe1f28f4ac844ca6
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-02-02 20:17:21 -0300

    Style: modal comments, Feat: list comment in modal

--- src/components/Post/CommentModal.tsx
+++ src/components/Post/CommentModal.tsx
@@ -6,6 +6,8 @@ import { Post  as PostType} from "types/Post"
 import { Button } from "../Button";
 import { FlashMessage } from "../FlashMessage";
 import { addComment } from "@/actions";
+import { GrClose } from "react-icons/gr";
+import Image from "next/image";
 
 interface CommentModalProps {
     post: PostType;
@@ -48,21 +50,48 @@ export default function CommentModal({ post, currentUserId, isOpen, onRequestClo
     }
   return (
     <Modal 
-        className={"w-[704px] mx-auto bg-white rounded border border-zinc-300"}
+        className={"w-[704px] mt-28 mx-auto bg-white rounded border border-zinc-300"}
         isOpen={isOpen} onRequestClose={onRequestClose} contentLabel="Comentarios" ariaHideApp={false}>
-            {flashMenssage && (
-                <FlashMessage message={flashMenssage.message} type={flashMenssage.type} />
-            )}
-            <p>Conteudo</p>
-            <div className="mb-4 flex flex-col gap-6">
-                <textarea 
-                    className="w-full h-32 p-2 border-zinc-300 rounded text-sm font-medium"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                    placeholder="Adicione um comentario"
-                ></textarea>
-                <div className="fl justify-end">
-                    <Button type="button" text="comentar" onClick={handleAddComment} />
+            <div className="p-4">
+                <div className="flex justify-between items-center">
+                    <h2 className="text-xl font-bold mb-4">
+                        Comentários
+                    </h2>
+                    <button className="bg-red-600 hover:bg-red-400 text-white p-2 rounded-full" onClick={onRequestClose}>
+                        <GrClose />
+                    </button>
+                </div>
+                {flashMenssage && (
+                    <FlashMessage message={flashMenssage.message} type={flashMenssage.type} />
+                )}
+                <div className="div mb-4 flex flex-col">
+                    {post.comments && post.comments.length > 0 ? (
+                        post.comments.map((comment) => (
+                            <div className="flex items-center mb-4" key={comment.id}>
+                                <Image 
+                                    src={comment.user.image || ''}
+                                    alt={`Imagem do usuário`}
+                                    width={40}
+                                    height={40}
+                                    className="w-10 h-10 object-cover rounded-full mr-3"
+                                />
+                                <p className="text-sm">
+                                    <strong>{comment.user.name}</strong> {comment.content}
+                                </p>
+                            </div>
+                        ))
+                    ) : ( <p>Nenhum comentário ainda...</p> )}
+                </div>
+                <div className="mb-4 flex flex-col gap-6">
+                    <textarea 
+                        className="w-full h-32 p-2 border border-zinc-300 rounded text-sm font-medium"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="Adicione um comentario"
+                    ></textarea>
+                    <div className="fl justify-end">
+                        <Button type="button" text="comentar" onClick={handleAddComment} />
+                    </div>
                 </div>
             </div>
     </Modal>
